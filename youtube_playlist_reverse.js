8630909e17a7f5c5045922c6a6df945547409d6f
Dragosarus <66659441+Dragosarus@users.noreply.github.com>
2020-06-09 13:16:36 +0000

    Updated version number

--- youtube_playlist_reverse.js
+++ youtube_playlist_reverse.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Play Youtube playlist in reverse order
 // @namespace    https://github.com/Dragosarus/Userscripts/
-// @version      1.0
+// @version      1.1
 // @description  Adds button for loading the previous video in a YT playlist
 // @author       Dragosarus
 // @match        www.youtube.com/watch?*list*
