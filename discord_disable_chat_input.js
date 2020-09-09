0075781f4d95d5423e7526ebe440676360f5fc15
Dragosarus <66659441+Dragosarus@users.noreply.github.com>
2020-09-09 00:13:13 +0000

    Now works when changing servers
    Added an extra observer to detect server changes (in which case the channel observer needs to be readded)

--- discord_disable_chat_input.js
+++ discord_disable_chat_input.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disable Discord chat input
 // @namespace    https://github.com/Dragosarus/Userscripts/
-// @version      1.0
+// @version      2.0
 // @description  Avoid accidentally typing in chat
 // @author       Dragosarus
 // @match        *discord.com/channels/*
@@ -17,20 +17,27 @@
     var disableTextArea = true;
     var menuId = GM_registerMenuCommand("Toggle (on)", toggle, 't');
 
-    const observer = new MutationObserver(observerCallback);
-    const options = {childList:true};
-    init();
+    const channelObserver = new MutationObserver(channelObserverCallback);
+    const serverObserver = new MutationObserver(serverObserverCallback);
+    const options = {childList:true, attributes:true};
+    addObserver(serverObserver,"div[class*='content-']");
+    addObserver(channelObserver,"div[class*='chat-']", disable);
 
-    function observerCallback(mutationList, observer) {
+    function serverObserverCallback(mutationList, observer) { // when changing servers
+        //console.log("server callback");
+        addObserver(channelObserver,"div[class*='chat-']", disable);
+    }
+    function channelObserverCallback(mutationList, observer) { // when changing channels
+        //console.log("channel callback");
         disable();
     }
 
-    function init() {
-        var chat = $("div[class*='chat']");
-        if (!chat.length) { setTimeout(init,100);}
+    function addObserver(observer, query, onSuccess = function(){}) {
+        var q = $(query);
+        if (!q.length) { setTimeout(function(){addObserver(observer, query, onSuccess)},100);}
         else {
-            observer.observe(chat[0], options);
-            disable();
+            observer.observe(q[0], options);
+            onSuccess();
         }
     }
 
@@ -51,16 +58,24 @@
         if (disableTextArea) {
             //console.log("disabled!");
             var textareaQuery = $("div[class*='slateTextArea']");
-            textareaQuery.attr("contenteditable","false");
-            textareaQuery[0].style.removeProperty("-webkit-user-modify"); // needed for Chrome
-            textareaQuery.parent().parent()[0].style.setProperty("pointer-events","none"); // disable mouse events
+            if (textareaQuery.length) {
+                textareaQuery.attr("contenteditable","false");
+                textareaQuery[0].style.removeProperty("-webkit-user-modify"); // needed for Chrome
+                textareaQuery.parent().parent()[0].style.setProperty("pointer-events","none"); // disable mouse events
+            } else {
+                setTimeout(disable,100);
+            }
         }
     }
 
     function enable() {
         var textareaQuery = $("div[class*='slateTextArea']");
-        textareaQuery.attr("contenteditable","true");
-        textareaQuery[0].style.setProperty("-webkit-user-modify", "read-write-plaintext-only");
-        textareaQuery.parent().parent()[0].style.removeProperty("pointer-events");
+        if (textareaQuery.length) {
+            textareaQuery.attr("contenteditable","true");
+            textareaQuery[0].style.setProperty("-webkit-user-modify", "read-write-plaintext-only");
+            textareaQuery.parent().parent()[0].style.removeProperty("pointer-events");
+        } else {
+            setTimeout(enable,100);
+        }
     }
 })();
