a27259a84291880ac7246779e49f26464b011a78
Dragosarus <66659441+Dragosarus@users.noreply.github.com>
2021-09-06 21:48:24 +0000

    Add option to display % progress (#2), refactoring
    The new option will take into account the durations of every (accessible) video in the playlist and display a percentage that either:
    - shows how much of the playlist the user has watched (percentageFormat = 0; default)
    - shows how much of the playlist the user has yet to watch (percentageFormat = 1)
    Note that it is disabled by default.
    
    Misc:
    - Inaccurate times are now by default preceded by ">" rather than "more than "; this is in order to reduce the text space occupied by the script
    - Values that do not change during execution are now declared as constants rather than variables
    - All selectors are now accessible through the `selectors` dictionary.

--- youtube_playlist_remaining_time.js
+++ youtube_playlist_remaining_time.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Display remaining Youtube playlist time
 // @namespace    https://github.com/Dragosarus/Userscripts/
-// @version      2.2
+// @version      3.0
 // @description  Displays the sum of the lengths of the remaining videos in a playlist
 // @author       Dragosarus
 // @match        http://www.youtube.com/*
@@ -19,39 +19,73 @@
      * 1: xhxmxs (e.g 25m2s or 25m 2s)
      * 2: h:mm:ss (e.g 25:02 or 1:00:31 or 0:03)
     */
-    var textformat = 1;
+    const showTime = true;
+    const timeFormat = 1;
 
-    var textformat0_decimalPlaces = 2;
-    var textformat0_hourThreshold = 3600; // e.g "3.50" hours instead of "210.00 minutes" or "12600 seconds"
-    var textformat0_minuteThreshold = 60; // e.g "2.50" minutes instead of "180.00 seconds"
-    var textformat1_spacing = true; // e.g 1h 23m 2s instead of 1h23m2s
-    var textformat1_forceFull = false; // e.g 0h3m2s instead of 3m2s, 3h0m50s instead of 3h50s, etc
-    var textformat2_forceFull = false; // e.g 0:03:02 instead of 3:02
+    /* Percentage formats:
+     * 0: % watched (e.g. [42% done])
+     * 1: % remaining (e.g. [58% left])
+    */
+    const showPercentage = false; // e.g. [42% done] (will be shown to the right of the time)
+    const percentageFormat = 0;
 
-    var includeCurrentVideo = true; // whether the length of the current video should be included
-    var before = " - "; // ::before
-    var before_miniplayer = " • ";
-    var after = " left) "; // e.g "15h 20m 15s left)"
-    var incompleteIndicator = "(more than "; // e.g "(more than 15h 20m 15s left)", for large playlists (> 200 videos)
-    var completeIndicator = "("; // e.g "(15h 20m 15s left)"
-    var updateCooldown = 2500; // limit how often update() is run (milliseconds)
+    const timeFormat0_decimalPlaces = 2;
+    const timeFormat0_hourThreshold = 3600; // e.g "3.50" hours instead of "210.00 minutes" or "12600 seconds"
+    const timeFormat0_minuteThreshold = 60; // e.g "2.50" minutes instead of "150.00 seconds"
+    const timeFormat1_spacing = true; // e.g 1h 23m 2s instead of 1h23m2s
+    const timeFormat1_forceFull = false; // e.g 0h3m2s instead of 3m2s, 3h0m50s instead of 3h50s, etc
+    const timeFormat2_forceFull = false; // e.g 0:03:02 instead of 3:02
 
+    const includeCurrentVideo = true; // whether the length of the current video should be included
+    const before = " - "; // ::before
+    const before_miniplayer = " • ";
+    const updateCooldown = 2500; // limit how often update() is run (milliseconds)
+
+    const time_after = " left) "; // e.g "15h 20m 15s left)"
+    const incompleteIndicator_time = "(>"; // e.g "(>15h 20m 15s left)", for large playlists (> 200 videos)
+    const completeIndicator_time = "("; // e.g "(15h 20m 15s left)"
+
+    const percentageFormat0_after = "% done]";
+    const percentageFormat1_after = "% left]";
+    const incompleteIndicator_percentage = " [~"; // e.g "(10s left) [~20% done]", for large playlists (> 200 videos)
+    const completeIndicator_percentage = " ["; // e.g "(10s left) [20% done]"
+    const percentage_decimalPlaces = 1;
+    const percentage_after = [percentageFormat0_after, percentageFormat1_after];
+
+
+    const DOWN = false; // direction
+    const UP = true; // direction
     var updateFlagTime = 0;
     var time_total_s = 0;
+    var time_total_s_elapsed = 0; // stores duration of previous videos
+    var direction_global = DOWN;
     var errorFlag = false;
-    var direction;
     var incompleteFlag = false; // A playlist only displays the 199 previous+next entries in the playlist.
+    var incompleteFlagR = false; // Used to determine if the percentage is accurate; checks the direction opposite to direction_global
     var miniplayerActive = false;
-    const DOWN = 0; // direction
-    const UP = 1; // direction
+
+    const selectors = {
+        "currentVideo": "#content ytd-playlist-panel-video-renderer[selected]",
+        "currentVideo_miniplayer": "div.miniplayer ytd-playlist-panel-video-renderer[selected]",
+        "drypt_label": "#drypt_label", // created by this script
+        "drypt_label_miniplayer": "#drypt_label_miniplayer", // created by this script
+        "playlistHeaderText": "div.index-message-wrapper",
+        "pytplir_btn": "#pytplir_btn", // https://greasyfork.org/en/scripts/404986-play-youtube-playlist-in-reverse-order
+        "timestamp": "span.ytd-thumbnail-overlay-time-status-renderer",
+        "unplayableText": "#unplayableText",
+        "vidCount": ".ytd-watch-flexy #playlist #publisher-container div yt-formatted-string",
+        "vidCount_miniplayer": "yt-formatted-string[id=owner-name] :nth-child(3)",
+        "vidNum": "#publisher-container span.index-message",
+        "vidNum_miniplayer": "yt-formatted-string[id=owner-name]",
+        "ytd_app": "ytd-app"
+    };
 
     const playlistObserver = new MutationObserver(observerCallback);
     const pytplirObserver = new MutationObserver(pytplirCallback);
-    const observerOptions = {subtree:true, childList:true, attributes:true, characterData:true};
+    const observerOptions = {attributes:true, characterData:true};
+
     initObservers(playlistObserver);
-
-    setInterval(check, updateCooldown);
-
+    setInterval(check, updateCooldown); // init; then ensure the pytplir button is detected correctly
 
     function observerCallback(mutationList, observer) {
        update();
@@ -62,47 +96,56 @@
 
     function initObservers(observer) {
         try {
-            observer.observe($(".ytd-watch-flexy #playlist").find("#publisher-container")[0], observerOptions);
-            observer.observe($("yt-formatted-string[id=owner-name]")[0], observerOptions); // miniplayer
+            observer.observe($(selectors.vidCount)[0], observerOptions);
+            observer.observe($(selectors.vidCount_miniplayer)[0], observerOptions); // miniplayer
         } catch (e) {
             setTimeout(function(){initObservers(observer)},100);
         }
     }
 
     function check() {
-        if (!$("#drypt_label").length || (miniplayerActive && !$("#drypt_label_miniplayer").length)) {
+        if (!$(selectors.drypt_label).length || (miniplayerActive && !$(selectors.drypt_label_miniplayer).length)) {
             update();
         }
-        if ($("#pytplir_btn").length) {
-            pytplirObserver.observe($("#pytplir_btn")[0],{attributes:true});
+        if ($(selectors.pytplir_btn).length) {
+            pytplirObserver.observe($(selectors.pytplir_btn)[0],{attributes:true});
         }
 
     }
 
     function update(force=false) {
-        if (Date.now() - updateFlagTime < updateCooldown && !force) {
+        var timeSinceUpdate = Date.now() - updateFlagTime;
+        if (timeSinceUpdate < updateCooldown && !force) {
+            setTimeout(update, updateCooldown - timeSinceUpdate);
             return;
         }
 
         updateFlagTime = Date.now();
-        direction = getDirection();
-        miniplayerActive = $("ytd-app")[0].hasAttribute("miniplayer-active_") || $("ytd-app")[0].hasAttribute("miniplayer-active");
+        miniplayerActive = $(selectors.ytd_app)[0].hasAttribute("miniplayer-active_") || $(selectors.ytd_app)[0].hasAttribute("miniplayer-active");
         var playlistEntry = getCurrentEntry();
         if (!playlistEntry) {return;}
 
+        direction_global = getDirection();
+        incompleteFlag = false;
+        incompleteFlagR = false;
         if (!includeCurrentVideo) {
-            playlistEntry = getNextEntry(playlistEntry);
+            playlistEntry = getNextEntry(playlistEntry, direction_global);
         }
+
         time_total_s = 0;
+        time_total_s_elapsed = 0;
         if (playlistEntry){
-            addTime(playlistEntry);
+            addTime(playlistEntry, direction_global);
         }
+        if (showPercentage) { // also need to sum the video durations in the other direction
+            addTime(playlistEntry, !direction_global);
+        }
+
         if (!errorFlag){
             display();
         } else {
             setTimeout(update,100);
             errorFlag = false;
-            incompleteFlag = false;
         }
     }
 
@@ -110,71 +153,77 @@
         var elem;
         try {
             if (miniplayerActive) {
-                return $("div.miniplayer").find("ytd-playlist-panel-video-renderer[selected]")[0];
+                return $(selectors.currentVideo_miniplayer)[0];
             } else {
-                return $("#content").find("ytd-playlist-panel-video-renderer[selected]")[0];
+                return $(selectors.currentVideo)[0];
             }
         } catch (e) {
             errorFlag = true;
         }
     }
 
-    function getNextEntry(current){
+    function getNextEntry(current, direction){
         var previous = current;
-        if (direction) { // pytplir
+        if (direction) {
             current = $(current).prev();
         } else {
             current = $(current).next();
         }
         if (current.length) {
-            if (!current.find("#unplayableText").prop("hidden")) {
-                return getNextEntry(current);
+            if (!current.find(selectors.unplayableText).prop("hidden")) {
+                return getNextEntry(current, direction);
             } else {
                 return current[0];
             }
         } else {
-            checkIncomplete(previous);
+            checkIncomplete(previous, direction);
             return undefined;
         }
     }
 
-    function checkIncomplete(entry) {
+    function checkIncomplete(entry, direction) {
         var vidNums = getVidNum();
         var num = parseInt($(entry).find("#index")[0].innerHTML);
         var currentVideo = isNaN(num) // ▶ instead of number
-        if (currentVideo){
-            incompleteFlag = false;
-        } else {
-            incompleteFlag = (direction == DOWN && num != vidNums[1]) || (direction == UP && num != 1);
+        if (!currentVideo){ // current video is neither the first nor the last video in the playlist
+            if (direction == direction_global) {
+                incompleteFlag = (direction_global == DOWN && num != vidNums[1]) || (direction_global == UP && num != 1);
+            } else {
+                incompleteFlagR = (direction_global == UP && num != vidNums[1]) || (direction_global == DOWN && num != 1);
+            }
         }
     }
 
     function getVidNum() { // returns integer array [current, total], e.g "32 / 152" => [32,152]
         var vidNum_tmp;
         if (miniplayerActive) {
-            vidNum_tmp = $("yt-formatted-string[id=owner-name]").children()[2].innerHTML;
+            vidNum_tmp = $(selectors.vidNum_miniplayer).children()[2].innerHTML;
         } else {
-            vidNum_tmp = $("#publisher-container").find("span.index-message")[0].innerHTML;
+            vidNum_tmp = $(selectors.vidNum)[0].innerHTML;
         }
         return vidNum_tmp.split(" / ").map(x => parseInt(x));
     }
 
     function getDirection(){ // Compatible with https://greasyfork.org/en/scripts/404986-play-youtube-playlist-in-reverse-order
-        var pytplir_btn = $("#pytplir_btn");
+        var pytplir_btn = $(selectors.pytplir_btn);
         if (!pytplir_btn.length) {
-            return DOWN; // 0
+            return DOWN;
         } else {
             return pytplir_btn.attr("activated") == "true" ? UP : DOWN;
         }
     }
 
-    function addTime(entry) {
+    function addTime(entry, direction) {
         var time_raw = getTime(entry);
         if (time_raw != "-1") {
-            time_total_s += hmsToSecondsOnly(time_raw);
-            entry = getNextEntry(entry);
+            if (direction == direction_global){
+                time_total_s += hmsToSecondsOnly(time_raw);
+            } else { // in order to calculate % done/remaining
+                time_total_s_elapsed += hmsToSecondsOnly(time_raw);
+            }
+            entry = getNextEntry(entry, direction);
             if (entry) {
-                addTime(entry);
+                addTime(entry, direction);
             }
         } else {
             errorFlag = true;
@@ -182,9 +231,9 @@
     }
 
     function getTime(item) {
-        var unavailable = !$(item).find("#unplayableText").prop("hidden");
+        var unavailable = !$(item).find(selectors.unplayableText).prop("hidden");
         if (!unavailable) {
-            var time = $(item).find("span.ytd-thumbnail-overlay-time-status-renderer");
+            var time = $(item).find(selectors.timestamp);
             if (!time.length) {// timestamp has not been loaded yet
                 return "-1";
             } else {
@@ -215,66 +264,88 @@
     function display() {
         var time = formatTime(time_total_s);
         if (time == "") {return;} // this is apparently possible
+        var timeString = "";
+        if (showTime) {
+            var time_before = incompleteFlag ? incompleteIndicator_time : completeIndicator_time; // e.g "(more than " or "( "
+            timeString = time_before + time + time_after;
+        }
 
-        var middle = incompleteFlag ? incompleteIndicator : completeIndicator; // e.g "(more than " or "( "
+        var percentageString = "";
+        if (showPercentage) {
+            var missingData = incompleteFlag || incompleteFlagR; // due to large playlist
+            var percentage_before = missingData ? incompleteIndicator_percentage : completeIndicator_percentage;
+            var playlistTime = time_total_s + time_total_s_elapsed;
+            var percentage;
+            switch (percentageFormat) { // determine numerator
+                case 0: // show % watched
+                    percentage = time_total_s_elapsed;
+                    break;
+                case 1: // show % remaining
+                    percentage = time_total_s;
+                    break;
+            }
+            percentage = (100 * percentage / playlistTime).toFixed(percentage_decimalPlaces);
+            percentageString = percentage_before + percentage + percentage_after[percentageFormat];
+        }
+
         if (!miniplayerActive) {
-            if (!$("#drypt_label").length) {
+            if (!$(selectors.drypt_label).length) {
                 var label = document.createElement("a");
                 var textColor = "rgb(237,240,243)";
                 label.setAttribute("font-family","Roboto, Noto, sans-serif");
                 label.setAttribute("font-size","13px");
                 label.setAttribute("fill",textColor);
                 label.setAttribute("id","drypt_label");
-                $("div.index-message-wrapper")[0].appendChild(label);
+                $(selectors.playlistHeaderText)[0].appendChild(label);
             }
-            $("#drypt_label")[0].innerHTML = before + middle + time + after;
+            $(selectors.drypt_label)[0].innerHTML = before + timeString + percentageString;
 
         } else { // miniplayer
-            if (!$("#drypt_label_miniplayer").length) {
+            if (!$(selectors.drypt_label_miniplayer).length) {
                 var label_miniplayer = document.createElement("a");
                 label_miniplayer.setAttribute("font-family","Roboto, Noto, sans-serif");
                 label_miniplayer.setAttribute("font-size","13px");
                 label_miniplayer.setAttribute("fill",textColor);
                 label_miniplayer.setAttribute("id","drypt_label_miniplayer");
-                $("yt-formatted-string[id=owner-name]")[0].appendChild(label_miniplayer);
+                $(selectors.vidNum_miniplayer)[0].appendChild(label_miniplayer);
             }
-            $("#drypt_label_miniplayer")[0].innerHTML = before_miniplayer + middle + time + after;
+            $(selectors.drypt_label_miniplayer)[0].innerHTML = before_miniplayer + timeString + percentageString;
         }
         incompleteFlag = false;
     }
 
     function formatTime(time_total_s) {
         var formats = [formatTime0, formatTime1, formatTime2];
-        if (textformat > formats.length || textformat < 0) {
-            textformat = 0;
+        if (timeFormat > formats.length || timeFormat < 0) {
+            timeFormat = 0;
         }
-        return formats[textformat](time_total_s);
+        return formats[timeFormat](time_total_s);
     }
 
     function formatTime0(time_total_s) { // "x.xx hours" OR "x.xx minutes" OR "x seconds"
-        if (time_total_s >= textformat0_hourThreshold) {
-            return (time_total_s / 3600).toFixed(textformat0_decimalPlaces) + " hours";
-        } else if (time_total_s >= textformat0_minuteThreshold) {
-            return (time_total_s / 60).toFixed(textformat0_decimalPlaces) + " minutes";
+        if (time_total_s >= timeFormat0_hourThreshold) {
+            return (time_total_s / 3600).toFixed(timeFormat0_decimalPlaces) + " hours";
+        } else if (time_total_s >= timeFormat0_minuteThreshold) {
+            return (time_total_s / 60).toFixed(timeFormat0_decimalPlaces) + " minutes";
         } else {
             return time_total_s + " seconds";
         }
     }
 
     function formatTime1(time_total_s) { // xhxmxs (e.g 25m2s or 25m 2s)
-        var space = textformat1_spacing ? " " : "";
+        var space = timeFormat1_spacing ? " " : "";
         var hh = Math.floor(time_total_s / 3600);
         var mm = Math.floor((time_total_s % 3600) / 60);
         var ss = time_total_s % 60;
 
         var text = "";
-        if (hh > 0 || textformat1_forceFull) {
+        if (hh > 0 || timeFormat1_forceFull) {
             text += hh + "h" + space;
         };
-        if (mm > 0 || textformat1_forceFull) {
+        if (mm > 0 || timeFormat1_forceFull) {
             text += mm + "m" + space;
         };
-        if (ss > 0 || textformat1_forceFull || !time_total_s) {
+        if (ss > 0 || timeFormat1_forceFull || !time_total_s) {
             text += ss + "s";
         };
         return text;
@@ -286,7 +357,7 @@
         var ss = time_total_s % 60;
 
         var text = "";
-        if (hh > 0 || textformat2_forceFull) {
+        if (hh > 0 || timeFormat2_forceFull) {
             text += hh + ":";
         }
         if (hh > 0 && mm > 0) { // 1:01:22
